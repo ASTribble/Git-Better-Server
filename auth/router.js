0f4956e249a357b441595ee148b6b9de355a4a9a
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-03-12 14:11:39 -0700

    added localStrategy to strategies.js, methods.validatePassword and statics.hashPassword to UserSchema, and localAuth to auth router

--- auth/router.js
+++ auth/router.js
@@ -4,3 +4,11 @@ const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 
 const router = express.Router();
+
+
+const localAuth = passport.authenticate('local', {session: false});
+
+router.post('/login', localAuth, (req, res) => {
+    console.log(`${req.user.username} successfully logged in.`);
+    return res.json({authToken});
+})
