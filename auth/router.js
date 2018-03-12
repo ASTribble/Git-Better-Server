93ac6b8a613d5f8ce7bbb2ece9f823744a3986bc
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-03-12 13:38:22 -0700

    Add createAuthToken and configure jwtAuth

--- auth/router.js
+++ auth/router.js
@@ -4,3 +4,14 @@ const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 
 const router = express.Router();
+
+const createAuthToken = (user) =>{
+  return jwt.sign({user}, config.JWT_SECRET, {
+    subject: user.username,
+    algorith: 'HS256',
+  });
+};
+
+const jwtAuth = passport.authenticate('jwt', {session: false});
+
+module.exports = {router};
