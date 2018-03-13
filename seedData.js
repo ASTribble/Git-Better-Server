7feef12e34988c72dbecbf99823b92e7bb53c85a
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-03-13 07:39:32 -0700

    added 5 seedData questions to seedData.js

---
+++ seedData.js
@@ -0,0 +1,27 @@
+[
+    {
+        "question": "What command would temporarily jump you back to commit 789abcd?",
+        "answer": "git checkout 789abcd"
+    },
+
+    {
+        "question": "What command would roll you back to a previous commit 789abcd while keeping the changes?",
+        "answer": "git reset --soft 789abcd"
+    },
+
+    {
+        "question": "What command will roll back the last commit?",
+        "answer": "git reset --soft HEAD~"
+    },
+
+    {
+        "question": "What command would you type if you want to permanently discard changes made after commit 789abcd?",
+        "answer": "git reset --hard 789abcd"
+    },
+
+    {
+        "question": "What command would you type if you want to discard changes made after the last commit?",
+        "answer": "git reset --hard HEAD~"
+    }
+]
+
