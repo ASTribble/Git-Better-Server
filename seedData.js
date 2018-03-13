4b88aad364c47ab75222588263983e2466b2f57e
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-03-13 07:57:21 -0700

    Add additional questions

--- seedData.js
+++ seedData.js
@@ -22,6 +22,25 @@
     {
         "question": "What command would you type if you want to discard changes made after the last commit?",
         "answer": "git reset --hard HEAD~"
-    }
+    },
+    {
+      "question": "How do you copy the commit 946992 from one branch to another?",
+      "answer":"git cherry-pick 946992"
+  },
+  {
+      "question":"How do you squash previous 5 commits without rebasing?",
+      "answer":"git reset --soft HEAD~5"
+  },
+  {
+      "question":"How do you clean untracked files interactively?",
+      "answer":"git clean -i"
+  },
+  {
+      "question":"How do you find out who changed the file index.js?",
+      "answer":"git blame index.js"
+  },
+  {
+      "question":"How do you unstage the file server.js that contains changes?",
+      "answer":"git reset server.js"
+  }
 ]
-
