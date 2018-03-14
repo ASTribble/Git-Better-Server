be4fb5a7010117d09a235476272d36adb2001d31
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-03-14 15:42:37 -0700

    added some comments to the put endpoint

--- questions/router.js
+++ questions/router.js
@@ -9,6 +9,8 @@ mongoose.Promise = global.Promise;
 
 const {DATABASE} = require('../config');
 
+//=================== router.get ==============================
+
 router.get('/', (req, res) => {
   const userID = req.user.id;
 
@@ -27,6 +29,7 @@ router.get('/', (req, res) => {
        currentQuestions = questions;
      }
     })
+    //change this to only send back currentQuestions[0] when it won't break the client
     .then(() => res.json(currentQuestions))
     .catch(err => {
       console.log('Error:', err);
@@ -35,6 +38,8 @@ router.get('/', (req, res) => {
   );
 });
 
+//========================== router.put =============================
+
 router.put('/', (req, res) => {
   //req.body { questionId: "21561345612", answer: "true" }
   // const {questionId, answer} = req.body;
@@ -42,21 +47,16 @@ router.put('/', (req, res) => {
   const userID = req.user.id;
 
   User.findById(userID)
-    //get the questions back from the user
-    .then(user => nextQuestions = user.questions)
+    .then(user => nextQuestions = user.questions)   //get the questions back from the user
     .then(() => {
-       //if the req.body.answer is true
-      if(req.body.answer){
-        //push to the end of the array the item that was in the front
-        nextQuestions.push(nextQuestions.shift()); 
+      
+      if(req.body.answer){     //if the req.body.answer is true
+        nextQuestions.push(nextQuestions.shift());  //push to the end of the array the item that was in the front
       }
-      else{ 
-        //if the answer was false, take the item that was at the front
-        // and splice it into index[1]
-        nextQuestions.splice(1, 0, nextQuestions.shift());
+      else {              //if the answer was false, take the item that was at the front 
+        nextQuestions.splice(1, 0, nextQuestions.shift());  // and splice it into index[1]
       }
-        //update the User with the new question list
-      return User.findByIdAndUpdate(userId, {questions: nextQuestions}, {new: true});
+      return User.findByIdAndUpdate(userId, {questions: nextQuestions}, {new: true});  //update the User with the new question list
     })
     .then(() => res.send('Next Questions saved and ready'))
     .catch(err => {
