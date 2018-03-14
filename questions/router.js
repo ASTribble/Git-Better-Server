0059566865b073fff5e9b0b669c25601310b2f11
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-03-14 13:55:03 -0700

    added questions to user schema. made get questions endpoint go through user schema before it returned the questions.

--- questions/router.js
+++ questions/router.js
@@ -1,29 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {Question} = require('./models');
-const {Users}
+const {User} = require('../users/models');
 const router = express.Router();
 const jsonParser = bodyParser.json();
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
 const {DATABASE} = require('../config');
 
 router.get('/', (req, res) => {
   const userID = req.user.id;
+  // console.log('user',req.user.id);
+  let currentQuestions;
 
   User.findById(userID)
     .then(user => user.questions)
-    .then(questions => 
-    console/log(questions));
-
-  return Question.find()
-    .then((q) => {
-      console.log(q);
-      return res.json(q);
+    .then(questions => {
+     if (questions < 1 || !questions){
+       return Question.find()
+       .then(qs => currentQuestions = qs)
+       .then(() => User.findByIdAndUpdate(userID, {questions: currentQuestions}, {new: true}))
+       .then(u => console.log('user after update:', u)) 
+     }
+     else {
+       currentQuestions = questions;
+     }
     })
-    .catch((err) => {
-      console.error(err);
+    .then(() => res.json(currentQuestions))
+    .catch(err => {
+      console.log('Error:', err);
       return res.send(500).json({message: 'Internal Server Error'});
-    });
+    }
+  );
 });
 
 router.put('/', (req, res) => {
