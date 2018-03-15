26eba330fe3c990760bc309b476516afa560de2c
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-03-15 14:25:35 -0700

    put logic seems to be working for false answer.  i still need to resave things to the user

--- questions/router.js
+++ questions/router.js
@@ -45,7 +45,7 @@ router.get('/', (req, res) => {
 
 router.get('/v2', (req, res) => {
   const userID = req.user.id;
-  console.log('userID')
+  console.log('userID', userID);
 
   User.findById(userID)
     .then(user => {
@@ -55,13 +55,22 @@ router.get('/v2', (req, res) => {
      if (user.questions < 1 || !user.questions){
         Question.find()
           .then(qs => {
+            // qs.forEach((q, i) => q.next = i + 1);
+            const newQuestions = qs.map((q, i) => ({...q.toObject(), next: i + 1}));
+            console.log('newQuestions', newQuestions);
+            newQuestions[newQuestions.length-1].next = null;
             User.findByIdAndUpdate( userID, 
-              ({questions: qs}, {head: 0}), 
+              {questions: newQuestions,
+              head: 0}, 
               {upsert: true, new: true})
-          .then(user => res.json(user.questions[user.head])) 
-            })
+          .then(user => {
+            console.log(user);
+            res.json(user.questions[user.head]); 
+          })
+          .catch(err => console.log('line 67 error', err));
+        })
       }
-      else if(!user.head){
+      else if(user.head === undefined || user.head === null){
           User.findByIdAndUpdate( userID, {head: 0}, {upsert:true, new:true})
           .then(user => res.json(user.questions[user.head]))
       } 
@@ -106,6 +115,91 @@ router.put('/', (req, res) => {
       console.log('Error:', err);
     });
 });
+
+//++++++++++++++++++++++++++ PUT REFACTOR +++++++++++++++++++++++++++++++++
+
+router.put('/v2', (req, res) => {
+  //req.body { questionId: "21561345612", answer: "true" }
+  // const {questionId, answer} = req.body;
+  console.log('req.body', req.body.answer);
+  let nextQuestions;
+  const answer = req.body.answer;
+  const userID = req.user.id;
+
+  // if the answer is true, we want to make user.questions[user.questions.length - 1].next = head
+ // and then move user.head to questions[user.head.next] OR if (!user.head.next) user.head++
+//basically, the logic for insertLast
+  User.findById(userID)
+    .then(user => {
+
+      const questions = user.questions;
+      console.log('questions before manipulation', questions)
+      let head = user.head;
+      console.log('head before ifs',head);
+
+      if(answer){
+        //insertLast(item)
+          let tempIndex = head;
+          let initialHead = head;
+
+          while(questions[tempIndex].next !== null){
+            tempIndex = questions[tempIndex].next;
+          } 
+          questions[tempIndex].next = head;
+          head = questions[head].next;
+          questions[initialHead].next = null,
+          console.log('questions in if answer', questions)
+      }
+      else {
+        //insertAt position 2 => head.next.next(.next)
+        const nextHead = questions[head].next;
+        let tempIndex = head;
+        let prevIndex;
+        for(let i = 0; i < 2; i++){
+          prevIndex = tempIndex;
+          tempIndex = questions[tempIndex].next;
+
+        }//tempIndex is now the value at head.next.next
+        questions[head].next = questions[tempIndex].next;
+        questions[tempIndex].next = head;
+        head = nextHead;
+        console.log('questions in if false answer', questions)
+      }
+      return {questions, head} 
+    })
+    .then(res => console.log('head questions passed in', res.head))
+    .catch(err => console.log('error in put/v2', err));     
+  }); 
+      ///////////////////////////////////
+
+    //     let tempNode = user.head;
+    //     while(array[tempNode].next !== null){
+    //         tempNode = array[tempNode].next;
+    //     }
+    //     tempNode.next = user.head;
+    //     user.head = head.next
+    // }
+
+    // }) 
+  
+//     .then(() => {
+      
+//       if(req.body.answer){     //if the req.body.answer is true
+//         nextQuestions.push(nextQuestions.shift());
+//         console.log('if happened');  //push to the end of the array the item that was in the front
+//       }
+//       else {              //if the answer was false, take the item that was at the front 
+//         nextQuestions.splice(1, 0, nextQuestions.shift());  // and splice it into index[1]
+//         console.log('else happened');
+//       }
+//       return User.findByIdAndUpdate(userID, {questions: nextQuestions}, {new: true});  //update the User with the new question list
+//     })
+//     //!!!!!!!! This response might break something... make it user.questions if need be!!!!
+//     .then(user => res.status(200).json(user.questions[0]))
+//     .catch(err => {
+//       console.log('Error:', err);
+//     });
+// });
 
 module.exports = {router};
 
