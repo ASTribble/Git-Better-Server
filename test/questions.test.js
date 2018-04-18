b9100be7e1624cf889f7a5d238f73e0f888dd3ee
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-04-18 13:14:28 -0700

    added test for adding questions to new user

--- test/questions.test.js
+++ test/questions.test.js
@@ -18,7 +18,8 @@ const questionsTestData = require('./questions-test-data');
 describe('Questions GET Endpoint', function(){
     
     const user = userTestData[0];
-    let authToken;
+    let userId,
+        authToken;
 
     beforeEach(function() {
         console.info('seeding question data');
@@ -27,7 +28,7 @@ describe('Questions GET Endpoint', function(){
             return User.hashPassword(user.password)
         })
         .then(password => {
-            console.log('Creating user');
+            console.info('Creating user');
 			return User.create({
 				username: user.username,
 				password: password,
@@ -36,23 +37,53 @@ describe('Questions GET Endpoint', function(){
             });
         })
         .then(res => {
-            console.log('res', res)
+            console.log('created user:', res);
+            userId = res._id;
             return chai.request(app)
             .post('/api/auth/login')
             .send({username: user.username, password: user.password})
             .then(res => {
                 console.log('should have authToken:', res.body)
-                authToken = res.body.authToken;   
+                authToken = res.body.authToken; 
+                return;  
             })
-        })
-        .catch(err => {
-            console.log('ERROR!!!!!!!!!!!!', err)
-        }); 
+        });
     });
 
     
-    it.only('Should run', function(){
-        expect(true).to.be.true;
+    it.only('Should add questions to new user', function(){
+        console.log('AUTH TOKEN', authToken);
+        return chai.request(app)
+            .get('/api/questions/v2')
+            .set('authorization', `Bearer ${authToken}`)
+            .then(() => {
+                return User.findById(userId);
+            })
+            .then(_user => {
+                console.log('user after questions added', _user);
+                expect(_user).to.have.property('questions').that.is.an('array');
+                
+                const questions = _user.questions;
+                expect(questions).to.not.be.empty;
+                expect(questions).to.have.lengthOf(5);
+                
+                expect(questions[0]).to.have.property('timesAsked', 0);
+                expect(questions[0]).to.have.property('correct', 0);
+                expect(questions[0]).to.have.property('question', 'This is index 0');
+                expect(questions[0]).to.have.property('answer', 'answer zero');
+                expect(questions[0]).to.have.property('next', 1);
+
+                expect(questions[4]).to.have.property('timesAsked', 0);
+                expect(questions[4]).to.have.property('correct', 0);
+                expect(questions[4]).to.have.property('question', 'This is index 4');
+                expect(questions[4]).to.have.property('answer', 'answer four');
+                expect(questions[4]).to.have.property('next', null);
+
+                expect(_user).to.have.property('head').that.is.a('number');  
+                expect(_user.head).to.deep.equal(0);
+            })
     });
+
+
 
 });
