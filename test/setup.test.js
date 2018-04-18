fad9cf81f5000e7eaba96b2602f024327531a338
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-04-18 15:08:31 -0700

    cleaned up and commented code in setup.test.js

--- test/setup.test.js
+++ test/setup.test.js
@@ -8,7 +8,6 @@ const expect = chai.expect;
 
 const {runServer, closeServer} = require('../index');
 const {TEST_DATABASE_URL} = require('../config');
-// const userTests = require('./users.test');
 
 chai.use(chaiHttp);
 
@@ -20,21 +19,26 @@ process.env.NODE_ENV = 'test';
 process.stdout.write('\x1Bc\n');
 
 
+//starts the server at the beginning of the tests running
 before(function() {
     console.log('running server');
     return runServer(TEST_DATABASE_URL);
 });
 
-//drops the database so there is a fresh start
+
+//drops the database after each 'it' block so there is a fresh start
 afterEach(function() {
     console.log('dropping database');
     return mongoose.connection.dropDatabase();
 });
 
+//shuts down the server after the tests complete
 after(function() {
     return closeServer();
 });
 
+
+// makes sure mocha and chai are running properly
 describe('Mocha and Chai', function() {
     it('should be properly setup', function() {
         expect(true).to.be.true;
